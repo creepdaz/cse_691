cf5d873280dfd71c947b584086265e47fa93c67a
creepdaz <dazzlingmei.86@gmail.com>
2020-05-06 23:10:37 -0400

    Update v2v_model.py

--- src/v2v_model.py
+++ src/v2v_model.py
@@ -16,15 +16,20 @@ from src.coarsening import *
 from src.graphCNN import *
 
 
-grid_side = 52
-number_edges = 8
+grid_side = 30
+number_edges = 10
 metric = 'euclidean'
+# layer 1 928 nodes 
 A = grid_graph(grid_side,number_edges,metric) # create graph of Euclidean grid
 
 # Compute coarsened graphs
 coarsening_levels = 1
 L, perm = coarsen(A, coarsening_levels)
-print(L)
+lmax=[]
+for i in range(coarsening_levels):
+    lmax.append(lmax_L(L[i]))
+
+
 
 class my_sparse_mm(torch.autograd.Function):
     """
@@ -49,101 +54,97 @@ class my_sparse_mm(torch.autograd.Function):
 
 
 
-
-# define the GCN layer using to extract the features in the graph nerual network
-#
-class GCNLayer(nn.Module):
-    def __init__(self, in_feats, out_feats):
-        super(GCNLayer, self).__init__()
-        self.linear = nn.Linear(in_feats, out_feats)
-
-    def forward(self, g, feature):
-        # Creating a local scope so that all the stored ndata and edata
-        # (such as the `'h'` ndata below) are automatically popped out
-        # when the scope exits.
-        with g.local_scope():
-            g.ndata['h'] = feature
-            g.update_all(gcn_msg, gcn_reduce)
-            h = g.ndata['h']
-            return self.linear(h)
-
-
-
-'''
-input shape is [12, 32, 44, 44, 44]
-'''
-class gcn_v(nn.Module):
-    def __init__(self, net_paras):
-        super(gcn_v,self).__init__()
-        D, CL1_k, CL1_f,out_feature = net_paras
-        # first still need conv3d 
-        self.conv1 = nn.Conv3d(1,1,7)
-        self.pool1 = nn.MaxUnpool3d(2)
-        #input feature, output feature 
-        self.cl1 = nn.Linear(CL1_k,CL1_f)
-        Fin =CL1_k
-        Fout= CL1_f
-        # define the init weight of self.cl1
-        # define the init bias of self.cl1
+class Graph_CNN(nn.Module):
+    
+    def __init__(self, net_parameters):
+        
+        
+        super(Graph_CNN, self).__init__()
+        
+        # parameters
+        D, CL1_F, CL1_K = net_parameters
+        
+        # graph CL1
+        self.cl1 = nn.Linear(CL1_K, CL1_F) 
+        Fin = CL1_K 
+        Fout = CL1_F
+        scale = np.sqrt( 2.0/ (Fin+Fout) )
         self.cl1.weight.data.uniform_(-scale, scale)
         self.cl1.bias.data.fill_(0.0)
-        self.CL1_k = CL1_k
-        self.CL1_f = CL1_f
-        self.bn= nn.BatchNorm1d(out_feature)
-
-
-    # cl is the linear combination of the fan out 
-    def graph_cheby(self,x,cl,bn,L,Fout,K):
-        #B: batch
-        #V: number of vertices
-        #Fin: number of input features
-        # Fout:number of output features
-        # K: Chebyshev orders and support  size
-
-        B, V, Fin = x.size()
-        B, V, Fin = int(B), int(V), int(Fin)
-
-        # transform to Chebyshev basis
-        x0 = x.permute(1, 2, 0).contiguous()  # V x Fin x B
-        x0 = x0.view([V, Fin * B])  # V x Fin*B
-        x = x0.unsqueeze(0)  
-
-        if torch.cuda.is_available():
-            L = L.cuda()
-        # concatenate the x and x_
-        def concat(x, x_):
-            x_ = x_.unsqueeze(0)            
-            return torch.cat((x, x_), 0)    
-        if K > 1: 
-            x1 = Calulate_grad()(L,x0)              # V x Fin*B
-            x = torch.cat((x, x1.unsqueeze(0)),0) 
+        self.CL1_K = CL1_K; self.CL1_F = CL1_F; 
         
-        for k in range(2, K):
-            x2 = 2 * Calulate_grad()(L,x1) - x0  
-            x = torch.cat((x, x2.unsqueeze(0)),0)  # M x Fin*B
-            x0, x1 = x1, x2 
         
-        x = x.view([K, V, Fin, B])  # K x V x Fin x B
-        x = x.permute(3, 1, 2, 0).contiguous()  # B x V x Fin x K
-        x = x.view([B * V, Fin * K])  # B*V x Fin
-
-        x = cl(x)
-
-        if bn is not None:
-            x = bn(x)  # B*V x Fout
-        x = x.view([B, V, Fout])  # B x V x Fout
-
-        return x  
-
-
+        # nb of parameters
+        nb_param = CL1_K* CL1_F + CL1_F          # CL1
+               
+        
     def init_weights(self, W, Fin, Fout):
 
         scale = np.sqrt( 2.0/ (Fin+Fout) )
         W.uniform_(-scale, scale)
 
         return W
+        
+        
+    def graph_conv_cheby(self, x, cl, L, lmax, Fout, K):
+        
+        print('k is ' ,K)
 
-        # Max pooling of size p. Must be a power of 2.
+        # parameters
+        # B = batch size
+        # V = nb vertices
+        # Fin = nb input features
+        # Fout = nb output features
+        # K = Chebyshev order & support size
+        B, V, Fin = x.size(); B, V, Fin = int(B), int(V), int(Fin) 
+
+        # rescale Laplacian
+        lmax = lmax_L(L)
+        L = rescale_L(L, lmax) 
+        
+        # convert scipy sparse matric L to pytorch
+        L = L.tocoo()
+        indices = np.column_stack((L.row, L.col)).T 
+        indices = indices.astype(np.int64)
+        indices = torch.from_numpy(indices)
+        indices = indices.type(torch.LongTensor)
+        L_data = L.data.astype(np.float32)
+        L_data = torch.from_numpy(L_data) 
+        L_data = L_data.type(torch.FloatTensor)
+        L = torch.sparse.FloatTensor(indices, L_data, torch.Size(L.shape))
+        L = Variable( L , requires_grad=False)
+        if torch.cuda.is_available():
+            L = L.cuda()
+        
+        # transform to Chebyshev basis
+        x0 = x.permute(1,2,0).contiguous()  # V x Fin x B
+        x0 = x0.view([V, Fin*B])            # V x Fin*B
+        x = x0.unsqueeze(0)                 # 1 x V x Fin*B
+        
+        def concat(x, x_):
+            x_ = x_.unsqueeze(0)            # 1 x V x Fin*B
+            return torch.cat((x, x_), 0)    # K x V x Fin*B  
+             
+        if K > 1: 
+            x1 = my_sparse_mm().apply(L,x0)              # V x Fin*B
+            x = torch.cat((x, x1.unsqueeze(0)),0)  # 2 x V x Fin*B
+        for k in range(2, K):
+            x2 = 2 * my_sparse_mm().apply(L,x1) - x0  
+            x = torch.cat((x, x2.unsqueeze(0)),0)  # M x Fin*B
+            x0, x1 = x1, x2  
+        
+        x = x.view([K, V, Fin, B])           # K x V x Fin x B     
+        x = x.permute(3,1,2,0).contiguous()  # B x V x Fin x K       
+        x = x.view([B*V, Fin*K])             # B*V x Fin*K
+        
+        # Compose linearly Fin features to get Fout features
+        x = cl(x)                            # B*V x Fout  
+        x = x.view([B, V, Fout])             # B x V x Fout
+        
+        return x
+        
+        
+    # Max pooling of size p. Must be a power of 2.
     def graph_max_pool(self, x, p): 
         if p > 1: 
             x = x.permute(0,2,1).contiguous()  # x = B x F x V
@@ -151,13 +152,54 @@ class gcn_v(nn.Module):
             x = x.permute(0,2,1).contiguous()  # x = B x V/p x F
             return x  
         else:
-            return x
-    # forward function of the graph network
-    def forward(self,x,d,L,lmax):
+            return x    
+        
+        
+    def forward(self, x, d, L, lmax):
+        
+        # graph CL1
+        x = x.unsqueeze(2) # B x V x Fin=1  
+        x = self.graph_conv_cheby(x, self.cl1, L[0], lmax[0], self.CL1_F, self.CL1_K)
+        x = F.relu(x)
+        x = self.graph_max_pool(x, 4)
+        
+            
         return x
+        
+        
+    def loss(self, y, y_target, l2_regularization):
+    
+        loss = nn.CrossEntropyLoss()(y,y_target)
 
+        l2_loss = 0.0
+        for param in self.parameters():
+            data = param* param
+            l2_loss += data.sum()
+           
+        loss += 0.5* l2_regularization* l2_loss
+            
+        return loss
+    
+    
+    def update(self, lr):
+                
+        update = torch.optim.SGD( self.parameters(), lr=lr, momentum=0.9 )
+        
+        return update
+        
+        
+    def update_learning_rate(self, optimizer, lr):
+   
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+
+        return optimizer
 
     
+    def evaluation(self, y_predicted, test_l):
+    
+        _, class_predicted = torch.max(y_predicted.data, 1)
+        return 100.0* (class_predicted == test_l).sum()/ y_predicted.size(0)
 
 
 
@@ -283,14 +325,27 @@ class V2VModel(nn.Module):
             Basic3DBlock(32, 32, 1),
         )
 
-        self.conv1= nn.Conv3d(32,32,1,1)
-        self.conv_graph = nn.Conv3d(32,1,1,1)
-        self.grap_pool = nn.MaxPool3d(3)
+        self.conv_graph = nn.Conv3d(1,1,1,1)
+        self.grap_pool = nn.MaxPool3d(4)
         self.ratio = 0.9
         self.output_layer = nn.Conv3d(32, output_channels, kernel_size=1, stride=1, padding=0)
         self.opt =False
-        self._initialize_weights()
+        self.fc1 = nn.Linear(10648,928)
+        # input is 32 out 
 
+        CL1_K = 32
+        CL1_F = 12
+        net_para = [928,CL1_F,CL1_K]
+        self.cl1 = nn.Linear(CL1_K, CL1_F) 
+        Fin = CL1_K 
+        Fout = CL1_F
+        scale = np.sqrt( 2.0/ (Fin+Fout))
+        self.cl1.weight.data.uniform_(-scale, scale)
+        self.cl1.bias.data.fill_(0.0)
+        self.CL1_K = CL1_K; 
+        self.CL1_F = CL1_F
+        self.fc2 = nn.Linear(12*232,32*22*22*22)
+        self.graphCNN= Graph_CNN(net_para)
 
     def _merge_x(self, a, b,ratio):
         a= Variable(a, requires_grad=True)
@@ -300,29 +355,36 @@ class V2VModel(nn.Module):
         return res
 
     def forward(self, x):
-        net_para = [1,2,3,4,5]
         # initialize the graph output of the current features
-        net_para = []
         x_=[]
         
         if self.opt:
             #input size is [12,32,44,44,44]
             x_ = x.clone()
-            # [12,1,44,44,44]
+            print("x_ size ", x_.shape)
             x_ = self.conv_graph(x) 
             x_= self.grap_pool(x_)
-            #[12 ,1 ,14,14,14]
-            x_ = x_.view(12,14*14*14)
+           
+            x_= x_.view(12,22*22*22)
+            x_ = self.fc1(x_)
+            # 928
+            drop_out = 0.5
+            net =  self.graphCNN
+            x_ = net.forward(x_,drop_out,L,lmax)
+            x_ = x_.view(12,12*232)
+        
+            x_ = self.fc2(x_)
 
-
-
+            print(' x shape !:' , x_.shape)
 
         else:
             x_ =Variable(self.conv1(x) , requires_grad=True)
         
-        
+        print(" x_ shape " ,x_.shape)
+        print("x shape", x.shape)        
         x = self.back_layers(x)
-        #x = x*self.ratio + x_*(1-self.ratio)
+        if(self.opt):
+            x = x*self.ratio + x_*(1-self.ratio)
         x = self.output_layer(x)
         #x = merge_x(x,x,0.9)
         return x
